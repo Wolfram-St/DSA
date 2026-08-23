72004b801471c14f50db71009bf111097800462a
Kinshuk Sharma <sharmakinshuk2006@gmail.com>
2026-08-23 22:28:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.56%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> brackets;
+
+        for(char c : s){
+            if(c == '(' || c == '{' || c == '['){
+                brackets.push(c);
+            }else{
+                if(brackets.empty()) return false;
+
+                char top = brackets.top();
+                if((c == ')' && top != '(') ||
+                   (c == '}' && top != '{')||
+                   (c == ']' && top != '[')){
+                    return false;
+                   }
+                brackets.pop();
+            }
+        }
+        return brackets.empty();
+        
+    }
+};
